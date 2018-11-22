4ac3d8fb19e72acf4611c15ca7addd2f1838a7e4
plazasoto <42448066+plazasoto@users.noreply.github.com>
2018-11-22 15:02:42 -0300

    Add files via upload

---
+++ 2018_2/desarrollo/icpc2018B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, t=0;
+	scanf("%d",&n);
+	int trees[n];
+	for (int i = 0; i < n; ++i){
+		scanf("%d",&trees[i]);
+		t+=trees[i];
+	}
+	t/=2;
+	for(int i = 0; i < n; ++i){
+		/* code */
+	}
+	//cout << t << endl;
+	return 0;
+}
