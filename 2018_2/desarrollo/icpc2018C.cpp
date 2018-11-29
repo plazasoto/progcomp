6dbca9b133366b1f611be4bb67b9aa3b9a72c1f0
plazasoto <42448066+plazasoto@users.noreply.github.com>
2018-11-29 14:57:04 -0300

    Add files via upload

---
+++ 2018_2/desarrollo/icpc2018C.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	int v[10000][2];
+	scanf("%d",&n);
+	for(int i = 0; i < n; ++i){
+		scanf("%d%d",&v[i][0],&v[i][1]);
+	}
+/*
+	for(int i = 0; i < n; ++i){
+		printf("%d %d\n",v[i][0],v[i][1]);
+	}
+*/
+	return 0;
+}
